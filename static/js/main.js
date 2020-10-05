cc109638ceea6153475a5227e42138826974d0d6
rouge13 <hammerjulien67@gmail.com>
2020-10-05 16:55:47 +0200

    modif navbar

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,3 @@
 window.onload = () => {
 
-
-    function myFunction() {
-        var x = document.getElementById("myNavbar");
-        if (x.className === "navbar") {
-            x.className += " responsive";
-        } else {
-            x.className = "navbar";
-        }
-    }
 }
