60a3335d82272e2bd7526059f359529004d190cf
rouge13 <hammerjulien67@gmail.com>
2020-10-05 22:49:44 +0200

    modif color addContact linker

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
-window.onload = () => {
+function hidePhoneNumber() {
+    var x = document.getElementById("phoneNumber");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
 
+function hideLocation() {
+    var x = document.getElementById("location");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
