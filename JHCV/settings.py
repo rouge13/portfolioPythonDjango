58144d0e5ea7e75710e88a192e74f6e4cc3938b0
rouge13 <62237564+rouge13@users.noreply.github.com>
2020-10-31 19:03:44 +0100

    modif home settings

--- JHCV/settings.py
+++ JHCV/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'JHCV.urls'
